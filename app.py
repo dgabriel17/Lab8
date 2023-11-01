339c1cee248e2d5dc8eb1b8bac149d2d4f5fd829
Gabriel Benavidez <gabeben3@gmail.com>
2023-11-01 03:00:36 -0700

    Changes to be committed: 	new file:   Enrollment exmaple data for Lab8.xlsx 	new file:   Lab 8.pptx 	new file:   __pycache__/app.cpython-311.pyc 	modified:   app.py 	new file:   creat.py 	new file:   instance/example.sqlite 	deleted:    static/css/labEight.css 	deleted:    static/js/labEight.js 	new file:   templates/index.html 	modified:   templates/login.html 	new file:   templates/signUpForm.html 	new file:   test.py 	new file:   vendor/animate/animate.css 	new file:   vendor/animsition/css/animsition.css 	new file:   vendor/animsition/css/animsition.min.css 	new file:   vendor/animsition/js/animsition.js 	new file:   vendor/animsition/js/animsition.min.js 	new file:   vendor/bootstrap/css/bootstrap-grid.css 	new file:   vendor/bootstrap/css/bootstrap-grid.css.map 	new file:   vendor/bootstrap/css/bootstrap-grid.min.css 	new file:   vendor/bootstrap/css/bootstrap-grid.min.css.map 	new file:   vendor/bootstrap/css/bootstrap-reboot.css 	new file:   vendor/bootstrap/css/bootstrap-reboot.css.map 	new file:   vendor/bootstrap/css/bootstrap-reboot.min.css 	new file:   vendor/bootstrap/css/bootstrap-reboot.min.css.map 	new file:   vendor/bootstrap/css/bootstrap.css 	new file:   vendor/bootstrap/css/bootstrap.css.map 	new file:   vendor/bootstrap/css/bootstrap.min.css 	new file:   vendor/bootstrap/css/bootstrap.min.css.map 	new file:   vendor/bootstrap/js/bootstrap.js 	new file:   vendor/bootstrap/js/bootstrap.min.js 	new file:   vendor/bootstrap/js/popper.js 	new file:   vendor/bootstrap/js/popper.min.js 	new file:   vendor/bootstrap/js/tooltip.js 	new file:   vendor/countdowntime/countdowntime.js 	new file:   vendor/css-hamburgers/hamburgers.css 	new file:   vendor/css-hamburgers/hamburgers.min.css 	new file:   vendor/daterangepicker/daterangepicker.css 	new file:   vendor/daterangepicker/daterangepicker.js 	new file:   vendor/daterangepicker/moment.js 	new file:   vendor/daterangepicker/moment.min.js 	new file:   vendor/jquery/jquery-3.2.1.min.js 	new file:   vendor/perfect-scrollbar/perfect-scrollbar.css 	new file:   vendor/perfect-scrollbar/perfect-scrollbar.min.js 	new file:   vendor/select2/select2.css 	new file:   vendor/select2/select2.js 	new file:   vendor/select2/select2.min.css 	new file:   vendor/select2/select2.min.js

--- app.py
+++ app.py
@@ -1,81 +1,123 @@
-import bcrypt
-from flask import Flask, render_template, request, jsonify  # Import what we need from the flask library
-from flask_sqlalchemy import SQLAlchemy  # Import sqlalchemy
-from flask_bcrypt import bcrypt
+from flask import Flask, redirect, url_for, render_template, request, jsonify, flash
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+import json
+from flask_login import LoginManager, current_user, login_user, login_required, logout_user, UserMixin, login_manager
+from flask_bcrypt import Bcrypt 
 
+#Configure Flask app
 app = Flask(__name__)
-# Bcript = bcrypt(app)
+bcrypt = Bcrypt(app)
+CORS(app)
 
-# Create the sql file for the database
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///creds.sqlite"
-db = SQLAlchemy(app)
+#Configure DB
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite" 
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.session_protection = "strong"
 
-# Create a class for the database
-class creds(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=False, nullable=False)
-    password = db.Column(db.String, unique=False, nullable=False)
+app.secret_key = 'keep it secret, keep it safe' # Add this to avoid an error
 
+#set db variable as a SQLAlchemy obj tied to flask app "app"
+db = SQLAlchemy(app) 
 
-# Create the database
-with app.app_context():
-    # db.drop_all()
-    db.create_all()
+class Users(UserMixin, db.Model):
+        id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+        username = db.Column(db.String, nullable=False)
+        password = db.Column(db.String, nullable=False)
 
-
-# Function to create a user in the database
-def new_user(username, password):
-
-    # Make the salt
-    mySalt = bcrypt.gensalt()
-
-    # Encode the password (need to do this before hashing)
-    password = password.encode()
-
-    # Encrypt/hash the password using the salt
-    password = bcrypt.hashpw(password, mySalt)
-
-    # Create a new user with the username and the encrypted password
-    this_user = creds(username=username, password=password)
-
-    # Add the user into the database
-    with app.app_context():
-        db.session.add(this_user)
-        db.session.commit()
+        def check_password(self, password):
+            return bcrypt.check_password_hash(self.password, password)
+            #return self.password == password
+        
+        def __repr__(self):
+            return '<User %r>' % self.username
+        
+        def get_id(self):
+           return (self.id)
 
 
-# Function to check if the password being inputted is the correct
-def check(input_pass, stored_pass):
+def showGradebookSelection(queryArr):
+        returnDict = {}
 
-    # Encode the inputted password
-    input_pass = input_pass.encode()
+        for student in queryArr: 
+                returnDict[getattr(student, 'name')] = getattr(student, 'grade')
 
-    # Encode the password that's in the database
-    stored_pass = stored_pass.encode()
+        return (returnDict)
 
-    # Return the value for if the password is correct or not
-    return bcrypt.checkpw(input_pass, stored_pass)
-
-
-# new_user("john@college.edu", "abc123") # STUDENT made for testing purposes
-# new_user("prof", "thisGUYbruh")
-
-value = check("abc123", "$2b$12$FNbUJximrOcJJnhFbKe6auC293jELQKdlC8y71tZPDxF81egnN51.")
-print(value)
-value = check("thisGUYbruh", "$2b$12$uUfafgly0tSNvQ.tHfqoM.FbV/I6RyPW7Mz/yqcMtT64tfd8gGrMm")
-print(value)
 
 @app.route('/')
-def main_page():
-    return render_template('login.html')
+def renderIndex():
+        #Display html file (**All html's go in templates -> CSS & JS go in static)
+        return render_template("login.html")
+
+@app.route('/success/<name>/<password>')
+def success(name, password):
+    _content = 'Welcome ' + name + '. Password: ' + password
+    return render_template("index.html", content = _content)
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
 
 
-# Login request
-# @app.route('/login'):
-#
-#     with app.app_context():
+@app.route('/logout', methods = ['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return render_template("login.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/login',methods = ['POST'])
+def login():
+
+    error = None
+    _username = request.form['name']
+    _password = request.form['password']
+
+    if current_user.is_authenticated:
+        return redirect(url_for('success',name = _username, password = _password))
+    user = Users.query.filter_by(username=_username).first()
+    if user is None or not user.check_password(_password):
+        error = "Invalid Username or Password"
+        flash("Incorrect Username or password")
+        return render_template("login.html", error = error)
+    else:
+        login_user(user)
+        return redirect(url_for('success',name = _username, password = _password))
+
+@app.route('/signUp')
+def signUp():
+    return render_template('signUpForm.html')
+
+@app.route('/createAccount', methods=['POST'])
+def createAccount():
+     if request.method == 'POST':
+        newName = request.form['newName']
+        newPassword = request.form['newPassword']
+
+        newPassword = bcrypt.generate_password_hash(newPassword).decode('utf-8') 
+
+        with app.app_context():
+
+            count = db.session.query(Users).count()
+            #Create new Gradebook object with value arguments
+            newStudent = Users(id = (count+1), username = newName, password=newPassword)
+
+            #Add new object to db
+            db.session.add(newStudent)
+
+            #Commit changes to db
+            db.session.commit()
+
+            print("added student")
+
+        print (newName + newPassword)
+
+        return render_template("login.html")
+
+
+if __name__ == '__main__':
+ app.run()
