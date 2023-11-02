3e5d8084112f213cf29c8f2e8cc3ce30bfd39717
dgabriel17 <damiangabriel5@outlook.com>
2023-11-02 12:13:05 -0700

    Working professor stuff

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import bcrypt
 from flask import Flask, render_template, request, jsonify  # Import what we need from the flask library
 from flask_sqlalchemy import SQLAlchemy  # Import sqlalchemy
 from flask_bcrypt import bcrypt
+from sqlalchemy import and_
+
 
 app = Flask(__name__)
 # Bcript = bcrypt(app)
@@ -12,69 +14,130 @@ db = SQLAlchemy(app)
 
 
 # Create a class for the database
-class creds(db.Model):
+class course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=False, nullable=False)
-    password = db.Column(db.String, unique=False, nullable=False)
+    courseName = db.Column(db.String, unique=False, nullable=False)
+    teacher = db.Column(db.String, unique=False, nullable=False)
+    time = db.Column(db.String, unique=False, nullable=False)
+    students = db.Column(db.String, unique=False, nullable=False)
+
+
+class roster(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course = db.Column(db.String, unique=False, nullable=False)
+    student = db.Column(db.String, unique=False, nullable=False)
+    grade = db.Column(db.Float, unique=False, nullable=False)
 
 
 # Create the database
 with app.app_context():
-    # db.drop_all()
+    db.drop_all()
     db.create_all()
 
+# Populate the db for testing purposes
+c1 = course(courseName="CSE 106", teacher="me", time="Thurs", students="20/20")
+c2 = course(courseName="CSE 107", teacher="you", time="Mon", students="3/20")
+c3 = course(courseName="CSE 111", teacher="me", time="Fri", students="0/20")
+c4 = roster(course="CSE 107", student="Carlos", grade=99.2)
+c5 = roster(course="CSE 111", student="Luis", grade=84.89)
+c6 = roster(course="CSE 106", student="Bodrul", grade=101.2)
 
-# Function to create a user in the database
-def new_user(username, password):
+with app.app_context():
+    db.session.add(c1)
+    db.session.add(c2)
+    db.session.add(c3)
+    db.session.add(c4)
+    db.session.add(c5)
+    db.session.add(c6)
+    db.session.commit()
 
-    # Make the salt
-    mySalt = bcrypt.gensalt()
 
-    # Encode the password (need to do this before hashing)
-    password = password.encode()
+@app.route('/<userName>')
+def main_page(userName):
+    name = "Welcome " + userName
+    return render_template('profClasses.html', header_text=name)
 
-    # Encrypt/hash the password using the salt
-    password = bcrypt.hashpw(password, mySalt)
 
-    # Create a new user with the username and the encrypted password
-    this_user = creds(username=username, password=password)
-
-    # Add the user into the database
+# Get all the professors classes
+@app.route('/<prof>/GET_CLASSES')
+def prof_classes(prof):
+    # Create an app context to perform the sql actions
     with app.app_context():
-        db.session.add(this_user)
+        # Perform the query to get all the data
+        with app.app_context():
+            data = course.query.filter_by(teacher=prof).all()
+
+        # Dictionary for holding the return data
+        return_data = {}
+
+        # Fill the dictionary with the data
+        for i, item in enumerate(data):
+            return_data[i] = {'Course': item.courseName, 'Professor': item.teacher, 'Time': item.time,
+                              'Capacity': item.students}
+            print(item.courseName)
+
+    # Return the whole dictionary and the proper API response
+    return jsonify(return_data), 200
+
+
+# Get all the students in the professors classes
+@app.route('/<prof>/GET_STUDENTS')
+def prof_students(prof):
+    # dictionary to hold the data we will return
+    student_data = {}
+
+    # Create App context to perform the sql queries
+    with app.app_context():
+        # Get the classes the prof teaches
+        mydata = course.query.filter_by(teacher=prof).all()
+
+        # Get the class names and store them in an array
+        classes = []  # Classes array
+        for item in mydata:
+            classes.append(item.courseName)
+
+        # Dictionary will hold all the data to be returned in the json response
+        return_data = []
+
+        # Perform the queries for each individual class in the roster database
+        for people in classes:
+            students = roster.query.filter_by(course=people).all()
+
+            for i, person in enumerate(students):
+                student_data = {
+                    'Course': person.course,
+                    'Name': person.student,
+                    'Grade': person.grade
+                }
+                # Append the student data to the combined data using a unique key (e.g., student's name)
+                return_data.append(student_data)
+
+    return jsonify(return_data), 200
+
+
+# Route to edit a student's grade
+@app.route('/<prof>/EDIT_GRADE/<student>', methods=['PUT'])
+def prof_edit(prof, student):
+
+    # Get the student's new grade value from the request
+    info = request.get_json()
+    new_grade = info.get('grade')           # Get the value for the new grade from the json request
+    student_class = info.get('class')       # Get the value for what class the student is in from the json request
+
+    # Make a new app context
+    with app.app_context():
+
+        # Find the student
+        student = roster.query.filter(and_(roster.course == student_class, roster.student == student)).first()
+
+        # Update the grade
+        student.grade = new_grade
+
+        # Commit the change to the database
         db.session.commit()
 
-
-# Function to check if the password being inputted is the correct
-def check(input_pass, stored_pass):
-
-    # Encode the inputted password
-    input_pass = input_pass.encode()
-
-    # Encode the password that's in the database
-    stored_pass = stored_pass.encode()
-
-    # Return the value for if the password is correct or not
-    return bcrypt.checkpw(input_pass, stored_pass)
-
-
-# new_user("john@college.edu", "abc123") # STUDENT made for testing purposes
-# new_user("prof", "thisGUYbruh")
-
-value = check("abc123", "$2b$12$FNbUJximrOcJJnhFbKe6auC293jELQKdlC8y71tZPDxF81egnN51.")
-print(value)
-value = check("thisGUYbruh", "$2b$12$uUfafgly0tSNvQ.tHfqoM.FbV/I6RyPW7Mz/yqcMtT64tfd8gGrMm")
-print(value)
-
-@app.route('/')
-def main_page():
-    return render_template('login.html')
-
-
-# Login request
-# @app.route('/login'):
-#
-#     with app.app_context():
+        # Return the change as a json response
+        return jsonify(student.grade), 200
 
 
 if __name__ == "__main__":
--- static/js/profView.js
+++ static/js/profView.js
@@ -2,17 +2,198 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     // Create vars for the buttons
-    const user_type = document.getElementById("user");          // If the user is a student or prof
-    const user_name = document.getElementById("username");      // Inputted username
-    const pass = document.getElementById("password");           // Inputted password
-    const login_button = document.getElementById("loginEnter"); // Login Button
+    const classesBtn = document.getElementById("classes");      // View classes button
+    const studentsBtn = document.getElementById("students");    // View students button
+    const updateBtn = document.getElementById("button_edit");   // Update grade button
+    const name_field = document.getElementById("name_edit");    // Name input
+    const grade_field = document.getElementById("grade_edit");  // Grade input
+    const class_field = document.getElementById("class_edit");  // Class input
 
-    // Add event listener for the button press for logining in
-    login_button.addEventListener("click", function() {
+    var myDict = {};
 
-        let url = 'http://127.0.0.1:5000/login/';
-        url = url + user_name.value;
+    // Add event listener for the button press for showing the classes a professor is teaching
+    classesBtn.addEventListener("click", function() {
 
+        // Do api call to get the data for the specific student
+        const request = new XMLHttpRequest();
+        let url = window.location.href;                         // Current URL value
+        url = url + "/GET_CLASSES";                                 // Update the url to match the route we want to take
+        request.open("GET", url)
+        request.send();
+        request.onload = () => {
 
+            // Only do the API call if we get a sucessful response code, else let the user know there was no student
+            if(request.status === 200) {
+                myDict = JSON.parse(request.response);
+            }
+
+            // Print out the items in the dictionary (testing purposes)
+            for (var key in myDict) {
+                var value = myDict[key];
+                console.log(key);
+                console.log(value);
+            }
+
+            // Print out the data from the json response
+            let i = 0;      // Looping var
+
+            // Make the html element
+            const tab = document.createElement("table");
+
+            const myTable = document.getElementById("class_table");
+            myTable.innerHTML = "";                             // Clear the html if it's already populated
+
+            // Make the first table row with the column names
+            let first = tab.insertRow(0);
+            let col1 = document.createElement("th");        // Create first table header element
+            let col2 = document.createElement("th");        // Create second table header element
+            let col3 = document.createElement("th");        // Create third table header element
+            let col4 = document.createElement("th");        // Create fourth table header element
+
+            col1.textContent = "Course";                    // Text for first header
+            col2.textContent = "Professor";                 // Text for second header
+            col3.textContent = "Time";                      // Text for third header
+            col4.textContent = "Capacity";                  // Text for fourth header
+
+            first.appendChild(col1);                        // Add the element to the row
+            first.appendChild(col2);                        // Add the element to the row
+            first.appendChild(col3);                        // Add the element to the row
+            first.appendChild(col4);                        // Add the element to the row
+
+            // Add all the data to the table
+            for (item in myDict) {
+                first = tab.insertRow(i+1);                // Create new row
+                col1 = document.createElement("td");        // Create new table data element
+                col2 = document.createElement("td");        // Create another table data element
+                col3 = document.createElement("td");        // Create another table data element
+                col4 = document.createElement("td");        // Create another table data element
+
+                col1.textContent = myDict[item].Course; // Write the student's name to the table element's value
+                col2.textContent = myDict[item].Professor;    // Write their grade to the table element's value
+                col3.textContent = myDict[item].Time;
+                col4.textContent = myDict[item].Capacity;
+
+                first.appendChild(col1);                    // Add the name element to the table
+                first.appendChild(col2);                    // Add the grade element to the table
+                first.appendChild(col3);
+                first.appendChild(col4);
+            }
+
+            // Add the final table to the html via and id
+            document.getElementById("class_table").appendChild(tab);
+        }
+
+        // Rest the dictionary
+        myDict = {};
     });
+
+    // Button for showing the students
+    studentsBtn.addEventListener("click", function() {
+
+        // Do api call to get the data for the specific student
+        const request = new XMLHttpRequest();
+        let url = ""
+        url = window.location.href;                         // Current URL value
+        url = url + "/GET_STUDENTS";                                 // Update the url to match the route we want to take
+        request.open("GET", url);
+        request.send();
+        request.onload = () => {
+
+            // Only do the API call if we get a sucessful response code, else let the user know there was no student
+            if(request.status === 200) {
+                myDict = JSON.parse(request.response);
+            }
+
+            // Print out the items in the dictionary (testing purposes)
+            for (var key in myDict) {
+                var value = myDict[key];
+                console.log(key);
+                console.log(value);
+            }
+
+
+        // Print out the data from the json response
+            let i = 0;      // Looping var
+
+            // Make the html element
+            const tab = document.createElement("table");
+
+            const myTable = document.getElementById("student_table");
+            myTable.innerHTML = "";                             // Clear the html if it's already populated
+
+            // Make the first table row with the column names
+            let first = tab.insertRow(0);
+            let col1 = document.createElement("th");        // Create first table header element
+            let col2 = document.createElement("th");        // Create second table header element
+            let col3 = document.createElement("th");        // Create third table header element
+
+            col1.textContent = "Course";                    // Text for first header
+            col2.textContent = "Name";                 // Text for second header
+            col3.textContent = "Grade";                      // Text for third header
+
+            first.appendChild(col1);                        // Add the element to the row
+            first.appendChild(col2);                        // Add the element to the row
+            first.appendChild(col3);                        // Add the element to the row
+
+            // Add all the data to the table
+            for (item in myDict) {
+                first = tab.insertRow(i+1);                // Create new row
+                col1 = document.createElement("td");        // Create new table data element
+                col2 = document.createElement("td");        // Create another table data element
+                col3 = document.createElement("td");        // Create another table data element
+
+                col1.textContent = myDict[item].Course; // Write the student's name to the table element's value
+                col2.textContent = myDict[item].Name;    // Write their grade to the table element's value
+                col3.textContent = myDict[item].Grade;
+
+                first.appendChild(col1);                    // Add the name element to the table
+                first.appendChild(col2);                    // Add the grade element to the table
+                first.appendChild(col3);
+            }
+
+            // Add the final table to the html via and id
+            document.getElementById("student_table").appendChild(tab);
+        }
+
+        // Rest the dictionary
+        myDict = {};
+    });
+
+    // Button for updating a grade
+    updateBtn.addEventListener ("click", function() {
+
+        // Rest the color of the text in case it was changed in the last button click
+        class_field.style.color = 'black';
+        name_field.style.color = 'black';
+        grade_field.style.color = 'black';
+
+        let url = window.location.href;
+        url = url + "/EDIT_GRADE/" + name_field.value;
+        console.log(name_field.value);
+        let xhttp = new XMLHttpRequest();
+        xhttp.open('PUT', url);
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        let newGrade = grade_field.value;
+        let currClass = class_field.value;
+        console.log(newGrade);
+
+        const body = {"grade": newGrade, "class": currClass};
+        xhttp.send(JSON.stringify(body));
+
+        // If the request was successful do nothing, if not, let the user know the was no student
+        xhttp.onload = () => {
+            if (xhttp.status === 200) {
+                console.log("done");
+            }
+            else {
+                 name_field.style.color = 'red';
+                 name_field.value = "Student not in grade book";
+                 grade_field.style.color = 'red';
+                 grade_field.value = "Student not in grade book";
+                 class_field.style.color = 'red';
+                 class_field.value = "Student not in grade book";
+            }
+        }
+    });
+
 });
